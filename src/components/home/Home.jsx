bc45dee493de5917b2d1a3538764bc6187f0d786
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2025-12-09 21:26:31 +0200

    update my CV

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
         <a
           target="_blank"
           rel="noreferrer"
-          href="https://drive.google.com/file/d/1mRLiEYUxDqbyqcMX-3mslbuBkWGWDU9F/view?usp=sharing"
+          href="https://drive.google.com/file/d/1ImXgOH-Ud4c8GmISA9vgimoiyWK0VJPC/view?usp=sharing"
         >
           <Button>
             Check Out My CV <RxResume size="15" />
