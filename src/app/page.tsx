9e08469d5cf047e40e9261f5b7424dcb3bef9a0a
Mahmoud Mohamed Gomaa <m.anamody10000@gmail.com>
2026-07-16 00:37:00 +0300

    adding text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/sections/Hero";
 
 export default function Home() {
   return (
-    <main className="flex-grow flex items-center justify-center">
+    <main className="grow flex items-center justify-center">
       <Hero />
     </main>
   );
